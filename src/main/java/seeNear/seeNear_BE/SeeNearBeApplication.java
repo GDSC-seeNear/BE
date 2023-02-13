bad2dddc236823b070887e8a4068a17c0ba11b12
happyjamy <juwhan1004@naver.com>
2023-02-13 13:43:30 +0900

    add decorator

--- src/main/java/seeNear/seeNear_BE/SeeNearBeApplication.java
+++ src/main/java/seeNear/seeNear_BE/SeeNearBeApplication.java
@@ -2,9 +2,12 @@ package seeNear.seeNear_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import seeNear.seeNear_BE.domain.auth.AuthService;
 
 @SpringBootApplication
+@ServletComponentScan
 @PropertySource("classpath:/app.properties")
 public class  SeeNearBeApplication {
 
