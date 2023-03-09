1c18720c5d93de3db6e6cae22fdecbe6a4532d09
happyjamy <juwhan1004@naver.com>
2023-03-09 17:05:39 +0900

    member entity change class to interface

--- src/main/java/seeNear/seeNear_BE/domain/Member/domain/Member.java
+++ src/main/java/seeNear/seeNear_BE/domain/Member/domain/Member.java
@@ -1,22 +1,20 @@
 package seeNear.seeNear_BE.domain.Member.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.sql.Date;
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class Member {
-    private int id;
 
-    private String phoneNumber;
-    private String name;
-    private Date birth;
-    private double addressLati;
-    private double addressLongi;
-    private String addressDetail;
-    private Boolean isConnect;
-    private Integer guardianId;
+public interface Member {
+    int id = 0;
+
+    String phoneNumber = null;
+    String name = null;
+    Date birth = null;
+    double addressLati = 0;
+    double addressLongi = 0;
+    String addressDetail = null;
+    Boolean isConnect = null;
+    Integer guardianId = null;
+
+    int getId();
+
+    String getName();
 }
