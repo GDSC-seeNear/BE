8a4e56896031f032a08c5145099192f3755eec58
happyjamy <juwhan1004@naver.com>
2023-03-29 21:44:39 +0900

    add default timeStamp

--- src/main/java/seeNear/seeNear_BE/domain/StatusCheck/domain/Status.java
+++ src/main/java/seeNear/seeNear_BE/domain/StatusCheck/domain/Status.java
@@ -1,11 +1,19 @@
 package seeNear.seeNear_BE.domain.StatusCheck.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import seeNear.seeNear_BE.domain.Chat.domain.Chat;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Status {
     @Id
@@ -16,10 +24,20 @@ public class Status {
 
     private boolean done;
 
+    @Column(name = "chat_id")
+    private int chatId;
+    @CreatedDate
+    private Timestamp createdAt;
+
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_id", insertable = false, updatable = false)
     @ToString.Exclude
     private Chat chat;
 
+    public Status(String type, boolean done, int chatId) {
+        this.type = type;
+        this.done = done;
+        this.chatId = chatId;
+    }
 }
