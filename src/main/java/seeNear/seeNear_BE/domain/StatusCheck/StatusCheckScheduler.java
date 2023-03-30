64b029097437124bf5deec8d374c7767646d6d05
happyjamy <juwhan1004@naver.com>
2023-03-30 18:58:34 +0900

    demo time change

--- src/main/java/seeNear/seeNear_BE/domain/StatusCheck/StatusCheckScheduler.java
+++ src/main/java/seeNear/seeNear_BE/domain/StatusCheck/StatusCheckScheduler.java
@@ -21,19 +21,19 @@ public class StatusCheckScheduler {
     public void sendAfternoonMealCheck() {
         statusCheckService.sendCheckAll("meal2", "점심 식사를 하셨나요?");
     }
-    @Scheduled(cron = "0 0 20 * * *")
+    @Scheduled(cron = "0 0 19 * * *")
     public void sendDinnerMealCheck() {
         statusCheckService.sendCheckAll("meal3", "저녁 식사를 하셨나요?");
     }
-    @Scheduled(cron = "0 0 20 * * *")
+    @Scheduled(cron = "0 10 19 * * *")
     public void sendHealthCheck () {
         statusCheckService.sendCheckAll("health", "컨디션은 괜찮으세요?");
     }
-    @Scheduled(cron = "0 0 20 * * *")
+    @Scheduled(cron = "0 10 19 * * *")
     public void sendFeelCheck () {
         statusCheckService.sendCheckAll("feel", "기분은 괜찮으세요?");
     }
-    @Scheduled(cron = "0 0 20 * * *")
+    @Scheduled(cron = "0 15 19 * * *")
     public void sendToiletCheck () {
         statusCheckService.sendCheckAll("toilet", "화장실은 다녀오셨나요?");
     }
